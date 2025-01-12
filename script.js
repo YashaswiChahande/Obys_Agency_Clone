8ba7a5cd808d8901ee5136f826229fff14a019e5
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-12 15:22:45 +0530

    loader completion_F

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
+//counter //property 'setInterval' loops after particular interval & //property 'Timeout' means delay
+//loader 
+
 var tl = gsap.timeline();
 tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5
+})
+
+tl.from("#line1-part1",{
+    opacity:0,
+    //inbuilt method to create function at start
+    onStart: function() {
+var h5timer = document.querySelector("#line1-part1 h5");
+var grow = 0;
+
+setInterval(function () {
+    if(grow < 100){
         h5timer.innerHTML = grow++;
-      } else {
+    }else{
         h5timer.innerHTML = grow;
-      }
-    }, 33);
-  },
+    }
+},33);
+    }
 });
-tl.to('.line h2',{
-    animationName: "anime",
-    opacity: 1
-})
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 4,
-});
-tl.from("#page1",{
-    delay: 0.2,
-    y: 1600,
-    opacity: 0,
-    duration: 0.5,
-    ease: Power4
-})
-tl.to("#loader",{
-    display: "none"
-})
+    tl.to(".line h2",{
+        animationName: "anime",
+        opacity: 1
+    })
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.2,
+        delay:4
+    })
+    tl.from("#page1",{
+        delay:0.2,
+        y:1600,
+        duration:0.5,
+        opacity:0,
+        ease: Power4
+    })
+    tl.to("#loader",{
+        display:"none"
+    })
